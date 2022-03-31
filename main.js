c00329f957205dd53734dacb724c5467ae0a5b37
AmarAdilovic <amaradilovic13@gmail.com>
2022-03-31 18:04:45 -0400

    Create UI elements and reformat logic

--- main.js
+++ main.js
@@ -1,90 +1,109 @@
+function uiElements(){
+    const container = document.querySelector(".container");
+
+    const rockButton = document.createElement("button");
+    rockButton.textContent = "Rock";
+
+    const paperButton = document.createElement("button");
+    paperButton.textContent = "Paper";
+
+    const scissorsButton = document.createElement("button");
+    scissorsButton.textContent = "Scissors";
+
+
+
+    container.appendChild(rockButton);
+    container.appendChild(paperButton);
+    container.appendChild(scissorsButton);
+
+}
+
+
 function computerPlay(){
     // Will return a random integer, either 1, 2, or 3
     let randInt = Math.floor(Math.random() * 3) + 1;
     if(randInt === 1)
-        return "rock";
+        return "Rock";
     else if(randInt === 2)
-        return "paper";
+        return "Paper";
     else if(randInt === 3)
-        return "scissors";
+        return "Scissors";
     
 }
 
 
-function playerPlay(){
-    let invalidSelection = true;
-    let playerSelection;
-    while(invalidSelection){
-        // The player selection will always be normalized to all lower case letters
-        playerSelection = (prompt("What would you like to do?: ")).toLowerCase();
-        // If there is an invalid selection, the while loop continues
-        if(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors"){
-            alert("Invalid entry, please try again. (Valid entries are \"Rock\", \"Paper\", or \"Scissors\".");
-        }
-        else{
-            invalidSelection = false;
-        }
-    }
-    return playerSelection;
-}
-
-
 function playRound(playerSelection, computerSelection){
+    console.log("Player selection: " + playerSelection);
+    console.log("Computer selection: " + computerSelection);
+
     // If there is a tie, a 2 is returned
     // If the player loses, a 0 is returned
     // If the player wins, a 1 is returned
     let winVal = -1;
     if(playerSelection === computerSelection)
         winVal = 2;
-    else if (playerSelection === "rock" && computerSelection === "paper")
+    else if (playerSelection === "Rock" && computerSelection === "Paper")
         winVal = 0;
-    else if (playerSelection === "rock" && computerSelection === "scissors")
+    else if (playerSelection === "Rock" && computerSelection === "Scissors")
         winVal = 1;
-    else if (playerSelection === "paper" && computerSelection === "scissors")
+    else if (playerSelection === "Paper" && computerSelection === "Scissors")
         winVal = 0;
-    else if (playerSelection === "paper" && computerSelection === "rock")
+    else if (playerSelection === "Paper" && computerSelection === "Rock")
         winVal = 1;
-    else if (playerSelection === "scissors" && computerSelection === "paper")
+    else if (playerSelection === "Scissors" && computerSelection === "Paper")
         winVal = 1;
-    else if (playerSelection === "scissors" && computerSelection === "rock")
+    else if (playerSelection === "Scissors" && computerSelection === "Rock")
         winVal = 0;
+
+    console.log("Win value: " + winVal);
 
     return([winVal, playerSelection, computerSelection]);
 }
 
-function capitalizeFirstLetter(string){
-    return string[0].toUpperCase() + string.substring(1);
-}
 
-function game(){
+function scoreCount(array){
     // playerResults and compResults are used to iterate wins for the player and computer respectively
     let playerResults = 0;
     let compResults = 0;
-    for(let i = 0; i < 5; i++){
-        // playRound returns an array with the results (array[0]),
-        // the player's selection array[1], and then the computer's selection array[2]
-        let array = playRound(playerPlay(), computerPlay());
 
-        if(array[0] === 0){
-            alert("You lost! " + capitalizeFirstLetter(array[2]) + " beats " + array[1] + ".");
-            compResults++;
-        }
-        else if(array[0] === 1){
-            alert("You won! " + capitalizeFirstLetter(array[1]) + " beats " + array[2] + ".");
-            playerResults++;
-        }
-        else if (array[0] === 2){
-            alert("You tied!");
-            playerResults += .5;
-            compResults += .5;
-        }
-        else{
-            alert("Error!");
-        }
+    if(array[0] === 0){
+        alert("You lost! " + array[2] + " beats " + array[1] + ".");
+        compResults++;
     }
+    else if(array[0] === 1){
+        alert("You won! " + array[1] + " beats " + array[2] + ".");
+        playerResults++;
+    }
+    else if (array[0] === 2){
+        alert("You tied!");
+        playerResults += .5;
+        compResults += .5;
+    }
+    else{
+        alert("Error!");
+    }
+    
     console.log("Final score: \nPlayer wins: " +
       playerResults + "\nComputer wins: " + compResults);
 }
 
+function game(){
+
+    // playRound returns an array with the results (array[0]),
+    // the player's selection array[1], and then the computer's selection array[2]
+    let array = [];
+    
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+       button.addEventListener('click', function(e){
+        console.log(e.target.innerText);
+        array = playRound(e.target.innerText, computerPlay())
+        scoreCount(array);
+        });
+    });
+}
+
+
+uiElements();
 // Call the gameplay loop
 game();
